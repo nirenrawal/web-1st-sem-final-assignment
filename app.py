88347b599fc240858f4d7688e63a7f8702071a6a
nirenrawal <nire0007@stud.kea.dk>
2022-04-24 23:14:36 +0200

    app.py modified

--- app.py
+++ app.py
@@ -119,8 +119,8 @@ def _():
 
 
 
-try:
-    import production
-    application = default_app()
-except:
-    run(host='127.0.0.1', port=2222, debug=True, reloader=True, server="paste")
+# try:
+#     import production
+#     application = default_app()
+# except:
+run(host='127.0.0.1', port=2222, debug=True, reloader=True, server="paste")
