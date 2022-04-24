677b6cb75894a49a56c89cd286510bc23ddec573
nirenrawal <nire0007@stud.kea.dk>
2022-04-24 23:20:04 +0200

    git ignore updated

---
+++ tailwindcss/tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  mode : "jit",
+  content: [
+    "../views/*.html"
+  ],
+  theme: {
+    extend: {
+      colors:{
+        "blue1": "#1DA1F2",
+        "blue2": "#2795D9",
+        "blue": "#EFF9FF",
+        "dark": "#657786",
+        "light": "#AAB8C2",
+        "lighter": "#E1E8ED",
+        "lightest": "#F5F8FA",
+      }    
+    },
+  },
+  plugins: [],
+}
